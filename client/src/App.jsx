bbf2a947d096563f0708c6fba4fe4a7145ff26e8
dani <dantesamuelrodriguez@gmail.com>
2024-11-20 11:09:52 -0500

    fixed styles

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Navbar from './components/Navbar'
+import Navbar from './components/layout/Navbar'
 import HomePage from './pages/HomePage'
 
 function App() {
