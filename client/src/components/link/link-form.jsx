23933549a62c08f6fd781f698f8bacd2e04f31b2
Dani <dantesamuelrodriguez@gmail.com>
2024-11-20 23:03:20 -0500

    add framer motion

--- client/src/components/link/link-form.jsx
+++ client/src/components/link/link-form.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
 import { Link2 } from 'lucide-react'
+import { motion, AnimatePresence } from 'framer-motion'
 
 export default function FormLink({ link, onClose }) {
   const [url, setUrl] = useState('')
@@ -23,92 +24,102 @@ export default function FormLink({ link, onClose }) {
   }
 
   return (
-    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
-      <form
-        onSubmit={handleSubmit}
-        className="bg-neutral-900 p-8 border border-neutral-700 rounded-lg w-full max-w-md"
+    <AnimatePresence>
+      <motion.div
+        className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
       >
-        <h1 className="text-2xl font-semibold text-white mb-6">
-          {link ? 'Edit Link' : 'Create new Link'}
-        </h1>
-        <div className="mb-4">
-          <label
-            htmlFor="url"
-            className="block text-sm font-medium text-gray-300"
-          >
-            URL
-          </label>
-          <input
-            type="text"
-            id="url"
-            value={url}
-            onChange={(e) => setUrl(e.target.value)}
-            className="mt-1 block w-full px-3 py-2 bg-neutral-900 border border-neutral-600 rounded-md text-white focus:outline-none focus:ring-2 focus:ring-neutral-500"
-          />
-        </div>
-        <div className="mb-4">
-          <label
-            htmlFor="short-link"
-            className="block text-sm font-medium text-gray-300"
-          >
-            Short link
-          </label>
-          <input
-            type="text"
-            id="short-link"
-            value={shortLink}
-            onChange={(e) => setShortLink(e.target.value)}
-            className="mt-1 block w-full px-3 py-2 bg-neutral-900 border border-neutral-600 rounded-md text-white focus:outline-none focus:ring-2 focus:ring-neutral-500"
-          />
-        </div>
-        <div className="mb-4">
-          <label
-            htmlFor="description"
-            className="block text-sm font-medium text-gray-300"
-          >
-            Description
-          </label>
-          <input
-            type="text"
-            id="description"
-            value={description}
-            onChange={(e) => setDescription(e.target.value)}
-            className="mt-1 block w-full px-3 py-2 bg-neutral-900 border border-neutral-600 rounded-md text-white focus:outline-none focus:ring-2 focus:ring-neutral-500"
-          />
-        </div>
-        <div className="mb-4">
-          <label
-            htmlFor="tag"
-            className="block text-sm font-medium text-gray-300"
-          >
-            Select your tag
-          </label>
-          <select
-            id="tag"
-            value={tag}
-            onChange={(e) => setTag(e.target.value)}
-            className="mt-1 block w-full px-3 py-2 bg-neutral-900 border border-neutral-600 rounded-md text-white focus:outline-none focus:ring-2 focus:ring-neutral-500"
-          >
-            <option value="1">1</option>
-            <option value="2">2</option>
-          </select>
-        </div>
-        <div className="flex justify-end gap-4">
-          <button
-            type="button"
-            onClick={onClose}
-            className="px-4 py-2 text-white rounded-md hover:bg-neutral-500 focus:outline-none focus:ring-2 transition-colors duration-500"
-          >
-            Cancel
-          </button>
-          <button
-            type="submit"
-            className="px-4 py-2 bg-neutral-800 text-white rounded-md flex items-center hover:bg-neutral-600 focus:outline-none focus:ring-2"
-          >
-            {link ? 'Update' : 'Create'} <Link2 width={18} className="ml-2" />
-          </button>
-        </div>
-      </form>
-    </div>
+        <motion.form
+          onSubmit={handleSubmit}
+          className="bg-neutral-900 p-8 border border-neutral-700 rounded-lg w-full max-w-md"
+          initial={{ scale: 0.8, opacity: 0 }}
+          animate={{ scale: 1, opacity: 1 }}
+          exit={{ scale: 0.8, opacity: 0 }}
+        >
+          <h1 className="text-2xl font-semibold text-white mb-6">
+            {link ? 'Edit Link' : 'Create new Link'}
+          </h1>
+          <div className="mb-4">
+            <label
+              htmlFor="url"
+              className="block text-sm font-medium text-gray-300"
+            >
+              URL
+            </label>
+            <input
+              type="text"
+              id="url"
+              value={url}
+              onChange={(e) => setUrl(e.target.value)}
+              className="mt-1 block w-full px-3 py-2 bg-neutral-900 border border-neutral-600 rounded-md text-white focus:outline-none focus:ring-2 focus:ring-neutral-500"
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              htmlFor="short-link"
+              className="block text-sm font-medium text-gray-300"
+            >
+              Short link
+            </label>
+            <input
+              type="text"
+              id="short-link"
+              value={shortLink}
+              onChange={(e) => setShortLink(e.target.value)}
+              className="mt-1 block w-full px-3 py-2 bg-neutral-900 border border-neutral-600 rounded-md text-white focus:outline-none focus:ring-2 focus:ring-neutral-500"
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              htmlFor="description"
+              className="block text-sm font-medium text-gray-300"
+            >
+              Description
+            </label>
+            <input
+              type="text"
+              id="description"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+              className="mt-1 block w-full px-3 py-2 bg-neutral-900 border border-neutral-600 rounded-md text-white focus:outline-none focus:ring-2 focus:ring-neutral-500"
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              htmlFor="tag"
+              className="block text-sm font-medium text-gray-300"
+            >
+              Select your tag
+            </label>
+            <select
+              id="tag"
+              value={tag}
+              onChange={(e) => setTag(e.target.value)}
+              className="mt-1 block w-full px-3 py-2 bg-neutral-900 border border-neutral-600 rounded-md text-white focus:outline-none focus:ring-2 focus:ring-neutral-500"
+            >
+              <option value="1">1</option>
+              <option value="2">2</option>
+            </select>
+          </div>
+          <div className="flex justify-end gap-4">
+            <button
+              type="button"
+              onClick={onClose}
+              className="px-4 py-2 text-white rounded-md hover:bg-neutral-500 focus:outline-none focus:ring-2 transition-colors duration-500"
+            >
+              Cancel
+            </button>
+            <button
+              type="submit"
+              className="px-4 py-2 bg-neutral-800 text-white rounded-md flex items-center hover:bg-neutral-600 focus:outline-none focus:ring-2"
+            >
+              {link ? 'Update' : 'Create'} <Link2 width={18} className="ml-2" />
+            </button>
+          </div>
+        </motion.form>
+      </motion.div>
+    </AnimatePresence>
   )
 }
